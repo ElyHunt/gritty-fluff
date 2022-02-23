baacf32c6332d24d7ab3b1bd2925466fb824f5c1
Elliot Hunt <35791781+ElyHunt@users.noreply.github.com>
2022-02-23 12:00:45 -0500

    Created a Camera LookAt Script and Debugged
    Fixed a Bug where player is levitating over the ground. Solution: Tighten up that Collider!
    
    Camera Lookat Script follows player now--movement doesn't work w/ 360 rotation

---
+++ Assets/CameraLookAt.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAt : MonoBehaviour
+{
+    public GameObject defaultFocus;
+    //Typically Player, controls what camera looks at by default. Doesn't change. Set in Editor.
+    private GameObject currentFocus;
+    //Typically Player, controls what camera looks at currently.
+    private GameObject holderFocus;
+    //Stores old focus for swapping back and forth.
+
+    //Initializes focus to default.
+    private void Start()
+    {
+        currentFocus = defaultFocus;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(currentFocus.transform);
+    } //look at currentFocus continually
+
+    //Looks at the default gameObject.
+    public void lookAtDefault()
+    {
+        holderFocus = defaultFocus;
+        swapFocus();
+    }
+    //Tells the camera to look at a new game object.
+    public void lookAtNew(GameObject focus)
+    {
+        holderFocus = focus;
+        swapFocus();
+    }
+
+    //Swaps back to what was being looked at previously. Useful for back-and-forth conversation.
+    public void swapFocus()
+    {
+        if(holderFocus != null)
+        {
+            GameObject temp = currentFocus;
+            currentFocus = holderFocus;
+            holderFocus = temp;
+        }
+    }
+}

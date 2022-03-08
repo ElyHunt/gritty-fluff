a0d9e97ea3869fa7ee308cfed9c20caa7b6ed118
Elliot Hunt <35791781+ElyHunt@users.noreply.github.com>
2022-03-08 14:03:24 -0500

    You can look around with mouse now
    A bit clunky.... opening an issue

--- Assets/CameraLookAt.cs
+++ Assets/CameraLookAt.cs
@@ -11,15 +11,38 @@ public class CameraLookAt : MonoBehaviour
     private GameObject holderFocus;
     //Stores old focus for swapping back and forth.
 
+    public float rotationSensitivity = 1f;
+
+    private float lastRotateY = 0;
+    private float lastRotateX = 0;
+
     //Initializes focus to default.
     private void Start()
     {
         currentFocus = defaultFocus;
+        this.transform.LookAt(currentFocus.transform.position);
     }
     // Update is called once per frame
     void Update()
     {
-        this.transform.LookAt(currentFocus.transform.position);
+       // this.transform.LookAt(new Vector3(currentFocus.transform.position.x, this.transform.forward.y, currentFocus.transform.position.z));
+
+
+        this.transform.Rotate(new Vector3(1, 0, 0), lastRotateY);
+        lastRotateY = (-Input.GetAxis("Mouse Y") * rotationSensitivity) + lastRotateY;
+        if (lastRotateY >= 90) lastRotateY = 90;
+        else if (lastRotateY <= -90) lastRotateY = -90;
+
+        this.transform.Rotate(new Vector3(0, 1, 0), lastRotateX);
+        lastRotateX = (Input.GetAxis("Mouse X") * rotationSensitivity) + lastRotateX;
+        if (lastRotateX >= 45) lastRotateX = 45;
+        else if (lastRotateX <= -45) lastRotateX = -45;
+
+
+        this.transform.LookAt(new Vector3(currentFocus.transform.position.x, currentFocus.transform.position.y, currentFocus.transform.position.z));
+
+        this.transform.Rotate(new Vector3(1,0,0), lastRotateY);
+        this.transform.Rotate(new Vector3(0,1,0), lastRotateX);
     } //look at currentFocus continually
 
     //Looks at the default gameObject.

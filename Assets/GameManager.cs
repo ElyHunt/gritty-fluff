46eb7aaa5793b3db433d73256e603f0b244ec24b
Elliot Hunt <35791781+ElyHunt@users.noreply.github.com>
2022-04-29 22:02:31 -0400

    Most basic features completed! Includes Game Win

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,19 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//Check if the game has been "won"! :)
 public class GameManager : MonoBehaviour
 {
     public GameObject gameWinScreen;
-    public GameObject[] checkBoxes;//UI Checkboxes
+    public GameObject inventory;//UI Checkboxes
+    private List<Image> checkBoxes;
     bool gameWon = false;
+
+    private void Start()
+    {
+        checkBoxes = new List<Image>();
+        Image[] inventoryElements = inventory.transform.GetComponentsInChildren<Image>();
+        for (int i = 0; i < inventoryElements.Length; i++) 
+            if (inventoryElements[i].CompareTag("CheckBoxes")) 
+                checkBoxes.Add(inventoryElements[i]);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        for(int i = 0; i < checkBoxes.Length; i++)
-        {
-            if (checkBoxes[i].GetComponent<Image>().enabled == true) gameWon = true;
-            else break;//if all checkBoxes are checked, you win! Otherwise, break!
-        }
+        gameWon = checkIfGameOver();
     }
+
+
+    private bool checkIfGameOver()
+    {
+        for (int i = 0; i < checkBoxes.Count; i++)
+        {
+            if (checkBoxes[i].enabled == false) return false;
+            //if all checkBoxes are checked, you win!
+        }
+        gameWinScreen.SetActive(true);
+        return true;
+    }
+
 }
